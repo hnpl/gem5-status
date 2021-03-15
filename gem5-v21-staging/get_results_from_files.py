ad4c09d2a14b6b084511ed8b5abc8521eb5858b7
Hoa Nguyen <hoanguyen@ucdavis.edu>
2021-03-15 09:54:13 -0700

    Add option to parse simout file
    This is useful when the info.json file failed to return the correct
    status, i.e. when the process has stopped running but the status
    is `Running`.
    
    Signed-off-by: Hoa Nguyen <hoanguyen@ucdavis.edu>

--- gem5-v21-staging/get_results_from_files.py
+++ gem5-v21-staging/get_results_from_files.py
@@ -15,6 +15,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Gathering boot-exit results')
     parser.add_argument('--input', type=str, help="Path to the results folder")
     parser.add_argument('--output', type=str, help="Path to the file containing parsed results", default="./results.csv")
+    parser.add_argument('--finished', action="store_true", default=False, help="Helps when all runs have finished, but info.json has \"Status\": \"Running\"")
     args = parser.parse_args()
 
     input_path = args.input
@@ -22,7 +23,7 @@ if __name__ == "__main__":
     #boot-exit/5.4.49/simple/8/MOESI_CMP_directory/init
     folder_structure = ['kernel', 'cpu_type', 'num_cpus', 'mem_sys', 'boot_type']
     with open(output_path, 'w') as f:
-        f.write('string, status, running, kill_reason, return_code\n')
+        f.write('string, status, running, kill_reason, return_code, simout_has_success\n')
         for dirname, filename in get_info_path_iterator(input_path):
             result = None
             filepath = os.path.join(dirname, filename)
@@ -30,9 +31,24 @@ if __name__ == "__main__":
                 result = json.load(g)
             if not result:
                 continue
-            try:            
-                to_write = "{},{},{},{},{}\n".format(result['string'], result['status'], result['running'], result['kill_reason'], result['return_code'])
-                f.write(to_write)
+            simout_has_success = "Unknown"
+            try:
+                to_write = "{},{},{},{},{}".format(result['string'], result['status'], result['running'], result['kill_reason'], result['return_code'])
+                if args.finished:
+                    simout_file = os.path.join(dirname, "simout")
+                    try:
+                        has_success = False        
+                        with open(simout_file, "r") as h:
+                            for line in h.readlines():
+                                line = line.strip()
+                                if line.startswith("Success!"):
+                                    has_success = True
+                                    break
+                        simout_has_success = str(has_success)
+                    except:
+                        print("warn: {} is invalid".format(simout_output_file))
+                to_write += ",{}".format(simout_has_success)
+                f.write(to_write+"\n")
             except KeyError:
                 print("warn: '{}' does not contain result".format(filepath))
         
